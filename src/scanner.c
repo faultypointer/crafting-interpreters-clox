2690d5218bb5f7247b0d7750009c1bbd82e1aaea
faultypointer <faultypointer@proton.me>
2024-11-17 20:13:55 +0545

    scanner complete?

--- src/scanner.c
+++ src/scanner.c
@@ -112,6 +112,75 @@ static Token number_token() {
   return make_token(TOKEN_NUMBER);
 }
 
+static bool is_alpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static TokenType check_keyword(int start, int len, const char *rest,
+                               TokenType type) {
+  if ((scanner.current - scanner.start == start + len) &&
+      memcmp(scanner.start + start, rest, len) == 0) {
+    return type;
+  }
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifier_type() {
+  switch (scanner.start[0]) {
+  case 'a':
+    return check_keyword(1, 2, "nd", TOKEN_AND);
+  case 'c':
+    return check_keyword(1, 4, "lass", TOKEN_CLASS);
+  case 'e':
+    return check_keyword(1, 3, "lse", TOKEN_ELSE);
+  case 'i':
+    return check_keyword(1, 1, "f", TOKEN_IF);
+  case 'n':
+    return check_keyword(1, 2, "il", TOKEN_NIL);
+  case 'o':
+    return check_keyword(1, 1, "r", TOKEN_OR);
+  case 'p':
+    return check_keyword(1, 4, "rint", TOKEN_PRINT);
+  case 'r':
+    return check_keyword(1, 5, "eturn", TOKEN_RETURN);
+  case 's':
+    return check_keyword(1, 4, "uper", TOKEN_SUPER);
+  case 'v':
+    return check_keyword(1, 2, "ar", TOKEN_VAR);
+  case 'w':
+    return check_keyword(1, 4, "hile", TOKEN_WHILE);
+  case 'f':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'a':
+        return check_keyword(2, 3, "lse", TOKEN_FALSE);
+      case 'o':
+        return check_keyword(2, 1, "r", TOKEN_FOR);
+      case 'u':
+        return check_keyword(2, 1, "n", TOKEN_FUN);
+      }
+    }
+    break;
+  case 't':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'h':
+        return check_keyword(2, 2, "is", TOKEN_THIS);
+      case 'r':
+        return check_keyword(2, 2, "ue", TOKEN_TRUE);
+      }
+    }
+    break;
+  }
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier_token() {
+  while (is_alpha(peek()) || is_digit(peek()))
+    advance();
+  return make_token(identifier_type());
+}
+
 Token scan_token() {
   skip_whitespace();
   scanner.start = scanner.current;
@@ -120,6 +189,8 @@ Token scan_token() {
     return make_token(TOKEN_EOF);
 
   char c = advance();
+  if (is_alpha(c))
+    return identifier_token();
   if (is_digit(c))
     return number_token();
   switch (c) {
